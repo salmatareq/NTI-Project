a155e8b2e56e18004dcca9c389941a3101e911e2
salma tarek <salmatarekamer@gmail.com>
2026-09-13 16:06:07 +0300

    edit profile

---
+++ JS/EditProfile.js
@@ -0,0 +1,75 @@
+const editDiv = document.querySelector(".edit-div");
+const editBtn = document.querySelector(".edit-profile");
+
+const saveBtn = document.getElementById("saveBtn");
+const cancelBtn = document.querySelector(".btn-outline-secondary");
+
+const nameInput = document.getElementById("name");
+const bioInput = document.getElementById("bio");
+const skillInput = document.getElementById("skillInput");
+const skillsContainer = document.getElementById("skillsContainer");
+
+const profileName = document.querySelector(".profile-details .name");
+const profileBio = document.querySelector(".profile-details .bio");
+
+editBtn.addEventListener("click", () => {
+  editDiv.hidden = false;
+});
+cancelBtn.addEventListener("click", () => {
+  editDiv.hidden = true;
+});
+const savedData = localStorage.getItem("profileData");
+
+if (savedData) {
+  const profileData = JSON.parse(savedData);
+
+  profileName.textContent = profileData.name;
+  profileBio.textContent = profileData.bio;
+
+  nameInput.value = profileData.name;
+  bioInput.value = profileData.bio;
+}
+saveBtn.addEventListener("click", () => {
+  const name = nameInput.value.trim();
+  const bio = bioInput.value.trim();
+
+  const skills = [];
+
+  document.querySelectorAll(".skill").forEach((skill) => {
+    skills.push(skill.childNodes[0].textContent.trim());
+  });
+
+  const profileData = {
+    name: name,
+    bio: bio,
+    skills: skills,
+  };
+
+  localStorage.setItem("profileData", JSON.stringify(profileData));
+
+  profileName.textContent = name;
+  profileBio.textContent = bio;
+  editDiv.hidden = true;
+});
+addSkillBtn.addEventListener("click", () => {
+  const skill = skillInput.value.trim();
+
+  if (skill === "") return;
+
+  const skillDiv = document.createElement("div");
+  skillDiv.classList.add("skill");
+
+  skillDiv.innerHTML = `
+        ${skill}
+        <button class="remove-skill" type="button">×</button>
+    `;
+
+  skillsContainer.appendChild(skillDiv);
+
+  skillInput.value = "";
+});
+skillsContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("remove-skill")) {
+    e.target.parentElement.remove();
+  }
+});
---
+++ JS/profile.js
@@ -0,0 +1,31 @@
+const uploadBtn = document.querySelector(".edit-image");
+const imageInput = document.getElementById("imageInput");
+const profileImage = document.querySelector(".profile-pic img");
+const profileImage2 = document.querySelector("#profileImage");
+;
+const savedImage = localStorage.getItem("profileImage");
+
+if (savedImage) {
+  profileImage.src = savedImage;
+  profileImage2.src = savedImage;
+}
+
+uploadBtn.addEventListener("click", () => {
+  imageInput.click();
+});
+
+imageInput.addEventListener("change", () => {
+  const file = imageInput.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      profileImage2.src = reader.result;
+
+      localStorage.setItem("profileImage", reader.result);
+    };
+
+    reader.readAsDataURL(file);
+  }
+});
